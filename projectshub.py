7cb357e3d6c4ebeaa320ff8f6975d389ff4e8599
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-02-15 16:46:14 +0530

    Login Page Test

--- projectshub.py
+++ projectshub.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
+@app.route('/login')
 def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def about():
-    return '<h1> About </h1>'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
